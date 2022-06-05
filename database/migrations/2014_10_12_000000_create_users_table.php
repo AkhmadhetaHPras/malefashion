bcb778fede76cc2c9903aa6fbfa333dba83a1aad
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2022-06-05 15:48:00 +0700

    membuat migration dan model beserta seeder untuk tabel users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('role', 20);
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('name', 50);
+            $table->string('telp', 15);
+            $table->string('gender', 10);
+            $table->date('birth');
+            $table->string('photo');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -4,17 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class OrderSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            UserSeeder::class,
-        ]);
+        //
     }
 }

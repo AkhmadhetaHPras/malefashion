d27fb421a511f7fad99478bf955a15583efcf52b
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2022-06-16 21:57:06 +0700

    img footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -254,7 +254,7 @@
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="footer__about">
                         <div class="footer__logo">
-                            <a href="#"><img src="img/logo-dark.webp" alt="" /></a>
+                            <a href="#"><img src="{{ asset('img/logo-dark.webp') }}" alt="" /></a>
                         </div>
                         <p>
                             The customer is at the heart of our unique business model, which
